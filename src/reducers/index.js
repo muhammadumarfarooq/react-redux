07cc8d67b628a7eada5b02abb1c875334cb7cbd6
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-01-09 21:00:51 +0500

    combining reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songReducer = () => {
   return [
     { title: "Sun Raha hai", duration: "22:03" },
@@ -11,3 +13,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
   if (action.type === "SONG_SELECTED") return action.payload;
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songReducer,
+  selectedSong: selectedSongReducer
+});
