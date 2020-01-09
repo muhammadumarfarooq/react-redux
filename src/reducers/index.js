61f06dd5fa48a02261f08e2b0f79b0479cde7ba1
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-01-09 20:51:32 +0500

    song reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+const songReducer = () => {
+  return [
+    { title: "Sun Raha hai", duration: "22:03" },
+    { title: "Dil ye mera", duration: "2:05" },
+    { title: "Me dhundne ko zamanay me", duration: "5:55" },
+    { title: "Dil diya gallah", duration: "3:12" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") return action.payload;
+  return selectedSong;
+};
